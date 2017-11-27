bec555913cf835afce4a9cf7f5ea2a707fcbd4d4
Joshua McFarren <jmcfarren@underlinecom.com>
2017-11-27 11:31:36 -0500

    sysargs and env

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # pyd3
 Python ID3 Updater: Uses Discogs API to update meta-data in your music library
-https://github.com/joshuamcfarren/pyd3.git
+[GitHub](https://github.com/joshuamcfarren/pyd3.git)
 
-http://mutagen.readthedocs.io/en/latest/
-https://www.discogs.com/developers/
+## Requirements
+
+[Mutagen](http://mutagen.readthedocs.io/en/latest/)
+[Discogs API](https://www.discogs.com/developers/)
+
+## Setup
+
+```bash
+$ echo 'export DISCOGS_USER_TOKEN="YOUR-TOKEN-HERE"' >> .env
+```
--- discogs.py
+++ discogs.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+import os
+import getopt
 import discogs_client
 
-def main():
-	user_agent = 'pyd3/0.1'
-	user_token = 'XXXXX'
-	d = discogs_client.Client(user_agent, user_token=user_token)
-	query = 'Amygdala (Roman Flugel Remix)'
-	qtype = 'release'
-	results = d.search(query, type=qtype)
-	release = results[0]
-	label = release.labels[0]
-	artist = release.artists[0]
-	catno = release.data['labels'][0]['catno']
-	print('Search: {!r} as {}'.format(query, qtype))
-	print('Release: {}'.format(release.title))
-	print('Artist:  {}'.format(artist.name))
-	print('Label:   {}'.format(label.name))
-	print('Cat#:    {}'.format(catno))
-	#print(dir(label))
+NAME = 'pyd3'
+VERSION = '0.1'
+URL = 'https://github.com/joshuamcfarren/pyd3.git'
+
+def main(argv):
+	try:
+		opts, args = getopt.getopt(argv,"r:",["release="])
+	except getopt.GetoptError:
+		print('{} -r <release>'.format(sys.argv[0]))
+		sys.exit(2)
+	user_agent = '{}/{} +{}'.format(NAME, VERSION, URL)
+	user_token = os.environ.get('DISCOGS_USER_TOKEN')
+	discogs = discogs_client.Client(user_agent, user_token=user_token)
+	for opt, arg in opts:
+		if opt in ("-r", "--release"):
+			query = arg
+			#query = 'Amygdala (Roman Flugel Remix)'
+			qtype = 'release'
+			results = discogs.search(query, type=qtype)
+			release = results[0]
+			label = release.labels[0]
+			artist = release.artists[0]
+			catno = release.data['labels'][0]['catno']
+			print('Request: {} {} {} {!r}'.format(user_agent, user_token, qtype, query))
+			print('Release: {}'.format(release.title))
+			print('Artist:  {}'.format(artist.name))
+			print('Label:   {}'.format(label.name))
+			print('Cat#:    {}'.format(catno))
+			#print(dir(label))
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
