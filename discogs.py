aea1fd80fe355e09c4f8600649cd42b06f8db230
Joshua McFarren <jmcfarren@underlinecom.com>
2017-12-07 10:50:43 -0500

    Added master release query

--- discogs.py
+++ discogs.py
@@ -5,37 +5,62 @@ import sys
 import os
 import getopt
 import discogs_client
+from pprint import pprint
 
 NAME = 'pyd3'
 VERSION = '0.1'
 URL = 'https://github.com/jmcfarren/pyd3.git'
+RELEASE_BASE_URL = 'https://www.discogs.com/release/'
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"r:",["release="])
+        opts, args = getopt.getopt(argv,'r:m:',['release=','master='])
     except getopt.GetoptError:
-        print('{} -r <release>'.format(sys.argv[0]))
-        sys.exit(2)
-    user_agent = '{}/{} +{}'.format(NAME, VERSION, URL)
-    user_token = os.environ.get('DISCOGS_USER_TOKEN')
-    discogs = discogs_client.Client(user_agent, user_token=user_token)
+        usage()
     for opt, arg in opts:
-        if opt in ("-r", "--release"):
+        if not arg:
+            usage()
+        else:
+            if opt in ('-r', '--release'):
+                qtype = 'release'
+            elif opt in ('-m', '--master'):
+                qtype = 'master'
+            else:
+                usage()
             query = arg
-            #query = 'Amygdala (Roman Flugel Remix)'
-            qtype = 'release'
+            user_agent = '{}/{} +{}'.format(NAME, VERSION, URL)
+            user_token = os.environ.get('DISCOGS_USER_TOKEN')
+            discogs = discogs_client.Client(user_agent=user_agent, user_token=user_token)
             results = discogs.search(query, type=qtype)
-            release = results[0]
-            label = release.labels[0]
-            artist = release.artists[0]
-            catno = release.data['labels'][0]['catno']
-            #print('Request: {} {} {} {!r}'.format(user_agent, user_token, qtype, query))
-            print('Release: {}'.format(release.title))
-            print('Artist:  {}'.format(artist.name))
-            print('Label:   {}'.format(label.name))
-            print('Cat#:    {}'.format(catno))
-            #print(dir(label))
+            if not results:
+                print('No results for {} search: {}'.format(qtype, query))
+            else:
+                if qtype == 'master':
+                    release = results[0].main_release
+                    label = release.labels[0]
+                    artist = release.artists[0]
+                    catno = release.data['labels'][0]['catno']
+                    year = release.data['released']
+                elif qtype == 'release':
+                    release = results[0]
+                    label = release.labels[0]
+                    artist = release.artists[0]
+                    catno = release.data['labels'][0]['catno']
+                    year = release.data['released']
+                print('Release: {}'.format(release.title))
+                print('Artist:  {}'.format(artist.name))
+                print('Label:   {}'.format(label.name))
+                print('Cat#:    {}'.format(catno))
+                print('Year:    {}'.format(release.year))
+                print('Discogs: {}{}'.format(RELEASE_BASE_URL, release.id))
 
 
-if __name__ == "__main__":
+def usage():
+    print('Usage:')
+    print("    {} -release 'search phrase'".format(sys.argv[0]))
+    print("    {} -master 'search phrase'".format(sys.argv[0]))
+    sys.exit(2)
+
+
+if __name__ == '__main__':
     main(sys.argv[1:])
